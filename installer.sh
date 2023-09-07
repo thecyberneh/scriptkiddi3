9b3af6dad8f4078d3a70c985b57e05f008c144a8
Neh Patel <75005384+thecyberneh@users.noreply.github.com>
2023-09-07 17:00:01 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,16 +9,16 @@ go install -v github.com/LukaSikic/subzy@latest
 go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
 unzip findomain-linux.zip  
-mv findomain $HOME/go/bin
-chmod 777 $HOME/go/bin/findomain
+mv findomain /$HOME/go/bin
+chmod 777 /$HOME/go/bin/findomain
 go install github.com/tomnomnom/httprobe@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 
 shc -f scriptkiddi3.sh   
 mv scriptkiddi3.sh.x scriptkiddi3
-cp scriptkiddi3 $HOME/go/bin
-chmod 777 $HOME/go/bin/scriptkiddi3
+cp scriptkiddi3 /$HOME/go/bin
+chmod 777 /$HOME/go/bin/scriptkiddi3
 rm -rf scriptkiddi3.sh   
 
 wget https://wordlists-cdn.assetnote.io/data/automated/httparchive_subdomains_2020_11_18.txt -O  subdomains.txt
